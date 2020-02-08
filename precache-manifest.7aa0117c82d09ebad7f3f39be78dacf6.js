4fb41ede20c402d245915557c72d19721149abb0
Sasha Kalinichenko <aleksandr.kalinichenko16@gmail.com>
2020-02-08 21:27:23 +0200

    Updates

---
+++ precache-manifest.7aa0117c82d09ebad7f3f39be78dacf6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d87ec776eac11dd0fc942d557650ab",
+    "url": "/choreography_prod/index.html"
+  },
+  {
+    "revision": "7ba7918f63248f53fea5",
+    "url": "/choreography_prod/static/css/main.120b3d52.chunk.css"
+  },
+  {
+    "revision": "714fa9bcd6142573c051",
+    "url": "/choreography_prod/static/js/2.4d64300f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/choreography_prod/static/js/2.4d64300f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ba7918f63248f53fea5",
+    "url": "/choreography_prod/static/js/main.91b12853.chunk.js"
+  },
+  {
+    "revision": "ca652ba2163a28bbe2db",
+    "url": "/choreography_prod/static/js/runtime-main.63edaf53.js"
+  },
+  {
+    "revision": "0f91b4b43db142beeaa556a65df4cd1c",
+    "url": "/choreography_prod/static/media/1.0f91b4b4.jpg"
+  },
+  {
+    "revision": "af5638a71af05b804aba879f5687ff5e",
+    "url": "/choreography_prod/static/media/1.af5638a7.jpg"
+  },
+  {
+    "revision": "8480665daa2b6e3cbcf4b032a00fb0ab",
+    "url": "/choreography_prod/static/media/2.8480665d.jpg"
+  },
+  {
+    "revision": "6d4dd64b4b511620a6d14c8f2a0c3844",
+    "url": "/choreography_prod/static/media/3.6d4dd64b.jpg"
+  }
+]);
