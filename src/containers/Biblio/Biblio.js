11f8328a33ac9d3b96416ceadf6f76b9d23247d9
Sasha Kalinichenko <aleksandr.kalinichenko16@gmail.com>
2020-02-09 16:20:54 +0200

    Text-fixes

--- src/containers/Biblio/Biblio.js
+++ src/containers/Biblio/Biblio.js
@@ -8,15 +8,15 @@ class Biblio extends React.Component{
                 <div className={classes.BiblioBlock+ " row"}>  
                 <div className="col-xl-6">              
                 <h1>Студентам</h1>
-                <h3><a href='https://drive.google.com/open?id=194ml1Oux9VsESQhafZP1BWHIQnVZX1kU' without='true' rel="noopener noreferrer" target='_blank' >Опорний конспект ктп</a></h3>
-                <Link to='/QuizList'><h3>Тести</h3></Link></div>
+                <h3><a href='https://drive.google.com/open?id=194ml1Oux9VsESQhafZP1BWHIQnVZX1kU' without='true' rel="noopener noreferrer" target='_blank' >Опорні конспекти</a></h3>
+                <Link to='/QuizList'><h3>Тестові матеріали</h3></Link></div>
                 <div className="col-xl-6">                <h1>Користні матеріали</h1>
                 <h3><a href='https://drive.google.com/open?id=1TTy3x9GQAmxVsUPMLX_W_w7bTFdP17x0' without='true' rel="noopener noreferrer" target='_blank'>Класичний танець</a></h3>
                 <h3><a href='https://drive.google.com/open?id=1q-xrY4ltyXOyraNBU9MyE-y80JsmcAza' without='true' rel="noopener noreferrer" target='_blank'>НСТ</a></h3>
-                <h3><a href='https://drive.google.com/open?id=1HhUyWD6gr9PgMRcrxh3pRZu-weiLQhvF'without='true' rel="noopener noreferrer" target='_blank'>Историко-бытовой танец ОАМ</a></h3>
-                <h3><a href='https://drive.google.com/open?id=1nv0Hpsm-ezW-HdqBEAOFt0z-jDtcop1I' without='true' rel="noopener noreferrer" target='_blank'>Современный и бальный танец</a></h3>
+                <h3><a href='https://drive.google.com/open?id=1HhUyWD6gr9PgMRcrxh3pRZu-weiLQhvF'without='true' rel="noopener noreferrer" target='_blank'>Историко-битовий танец ОАМ</a></h3>
+                <h3><a href='https://drive.google.com/open?id=1nv0Hpsm-ezW-HdqBEAOFt0z-jDtcop1I' without='true' rel="noopener noreferrer" target='_blank'>Сучасний та балетний танець</a></h3>
                 <h3><a href='https://drive.google.com/open?id=1mNBVLvE545a2M59u2j9TZXUcC6DYwGv0' without='true' rel="noopener noreferrer" target='_blank'>Гимнастика медицина</a></h3>
-                <h3><a href='https://drive.google.com/open?id=16ArUGwAC3JOVvzYsMSXvfZitXXtjgPUG' without='true' rel="noopener noreferrer" target='_blank'>Разное</a></h3>
+                <h3><a href='https://drive.google.com/open?id=16ArUGwAC3JOVvzYsMSXvfZitXXtjgPUG' without='true' rel="noopener noreferrer" target='_blank'>Різне</a></h3>
                 <h3><a href="https://drive.google.com/file/d/1YEDwtZ3bDVpRmIJGlxqseUK-OPlQ6Wo1/view" without='true' rel="noopener noreferrer" target="_blank">Завантажити відео</a></h3>
                 </div></div>
 
